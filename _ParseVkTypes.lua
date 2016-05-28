7fd1b0f1e8b2bad54de3e7f20ca461e2a4a72035
alfonse <none@none>
2016-05-28 13:44:02 -0400

    Rearchitected ParseVkTypes. Added `define` framework, but no implementation.

--- _ParseVkTypes.lua
+++ _ParseVkTypes.lua
@@ -1,11 +1,15 @@
 
 local parse_dom = require "_ParseVkDom"
 
+local Tests = {}
+local Procs = {}
 
-local funcs = {}
+function Tests.Include(node)
+	return node.name == "type" and node.attr.category == "include"
+end
 
-function funcs.Include(node, output)
-	local data = {}
+function Procs.Include(node)
+	local data = {kind = "include"}
 	
 	--Parse the include name. May be an attribute `name` or a `name` child element.
 	if(node.attr.name) then
@@ -41,11 +45,50 @@ function funcs.Include(node, output)
 		data.style = "quote"
 	end
 	
-	print(data.name, data.need_ext, data.style)
+	return data
+end
+
+function Tests.Require(node)
+	return node.name == "type" and node.attr.category == nil and node.attr.requires
+end
+
+function Procs.Require(node)
+	local data = { kind = "reference" }
 	
-	if(output) then
-		output[#output + 1] = data
+	data.name = node.attr.name
+	data.include = node.attr.requires
+	
+	return data
+end
+
+--If the `name` is an attribute, then the entire accumulated text child
+--is a massive C-expression, which replaces any definition.
+--Otherwise, must part #define <name/> c-expression.
+--
+--Must check to see if there are comments to be extracted into comment fields.
+--The c-expression may contain `type` elements. These must be extracted into `requires` clauses.
+--The c-expression may be an integer value. Should be extracted into a simple value expression.
+function Procs.Define(node)
+end
+
+
+local funcs = {}
+
+
+function funcs.GenProcTable(StoreFunc)
+	local procTable = {}
+	for name, test in pairs(Tests) do
+		local procEntry = {}
+		procTable[#procTable + 1] = procEntry
+		procEntry.Test = test
+		local Proc = assert(Procs[name])
+		
+		procEntry.Proc = function(node, ...)
+			local data = Proc(node)
+			StoreFunc(data, ...)
+		end
 	end
+	return procTable
 end
 
 return funcs
